e2e3a2195c50728d7c92cd12e90eb8b07e3bc259
JKWTCN <jkwtcn@icloud.com>
2024-10-26 19:25:07 +0800

    优化项目结构

---
+++ setting.py
@@ -0,0 +1,20 @@
+class setting:
+    developers_list = [1505617447, 3070004098]
+    admin_list = [
+        2781865679,
+        3011745967,
+        3835689054,
+        2362647500,
+        2239158081,
+        921065312,
+        1580584754,
+        1286145524,
+        692309296,
+    ]
+    bot_id = 3443801350
+    admin_group_main = 868030377
+    admin_group_list = [868030377,116177178]
+    # miaomiao群友
+    miaomiao_group_member = 2325333054
+    test_group = 116177178
+    timeout = 30
