e7e87367a08557661e2140ba00b92f97271fede7
JKWTCN <jkwtcn@icloud.com>
2024-11-22 21:32:06 +0800

    更新sh文件。

--- restart.sh
+++ restart.sh
@@ -1,2 +1,4 @@
 sudo kill -9 `lsof -ti:27431`
+sudo kill -9 `lsof -ti:27432`
 nohup /home/orangepi/bot/.venv/bin/python main.py > log/output.log
+nohup /home/orangepi/bot/.venv/bin/python main_chat.py > log/chat_output.log
