860727894048bbd2b01b1f57978eca81c070f2fd
JKWTCN <jkwtcn@icloud.com>
2024-12-15 15:59:48 +0800

    feat(main): 更新用户晋升成功消息并添加积分清零提示
    - 在用户晋升成功时，添加了积分清零的提示信息
    - 优化了晋升成功消息的格式，使其更加清晰和完整

--- main.py
+++ main.py
@@ -269,7 +269,7 @@ async def echo(websocket):
                                         say(
                                             websocket,
                                             group_id,
-                                            f"晋升成功,{get_user_name(user_id,group_id)}({at_id})的等级提升为{get_level(at_id, group_id)}级",
+                                            f"晋升成功,{get_user_name(user_id,group_id)}({at_id})的等级提升为{get_level(at_id, group_id)}级,积分清零喵。",
                                         )
                                     elif HasKeyWords(
                                         raw_message, ["惩罚取消", "取消惩罚"]
