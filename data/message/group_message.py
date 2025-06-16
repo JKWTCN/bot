517ed4e17fe4ebd1c61024c830cc81b034c3b975
JKWTCN <jkwtcn@icloud.com>
2025-06-16 23:27:30 +0800

    更新结构；

--- data/message/group_message.py
+++ data/message/group_message.py
@@ -1,4 +1,4 @@
-from message_post_type import MessagePostType
+from message_post_type import MessagePostType, string2MessagePostType
 from message_info import MessageInfo
 
 
@@ -13,11 +13,9 @@ class GroupMesssageInfo(MessageInfo):
     replyMessageId = -1
     imageFileList = []
 
-    def __init__(self):
-        super().__init__()
-        self.messagePostType = MessagePostType.string2MessagePostType(
-            self.rawMessage["post_type"]
-        )
+    def __init__(self, rawMessage: dict):
+        super().__init__(rawMessage)
+        self.messagePostType = string2MessagePostType(self.rawMessage["post_type"])
         self.groupId = self.rawMessage["group_id"]
         self.senderId = self.rawMessage["user_id"]
         self.messageId = self.rawMessage["message_id"]
--- data/message/message_post_type.py
+++ data/message/message_post_type.py
@@ -6,17 +6,18 @@ class MessagePostType(Enum):
     NOTICE = 2
     REQUEST = 3
     META_EVENT = 4
-    UNKONW_EVENT = 5
+    UNKNOWN_EVENT = 5
 
-    def string2MessagePostType(messagePostTypeString: str):
-        match messagePostTypeString:
-            case "message":
-                return MessagePostType.MESSAGE
-            case "notice":
-                return MessagePostType.NOTICE
-            case "request":
-                return MessagePostType.REQUEST
-            case "meta_event":
-                return MessagePostType.META_EVENT
-            case _:
-                raise MessagePostType.UNKONW_EVENT
+
+def string2MessagePostType(messagePostTypeString: str) -> MessagePostType:
+    match messagePostTypeString:
+        case "message":
+            return MessagePostType.MESSAGE
+        case "notice":
+            return MessagePostType.NOTICE
+        case "request":
+            return MessagePostType.REQUEST
+        case "meta_event":
+            return MessagePostType.META_EVENT
+        case _:
+            return MessagePostType.UNKNOWN_EVENT
