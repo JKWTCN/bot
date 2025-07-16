15fbbd2d74fa6e62e07faf484a51c85ca4ab207f
JKWTCN <jkwtcn@icloud.com>
2025-07-17 07:58:08 +0800

    feat：增加日志的可读性；

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -6,6 +6,9 @@ import schedule.application_list as application_list
 from data.enumerates import MessageType, ApplicationCostType, ApplicationType
 from schedule.consuming_high_time_queue import consuming_time_process_queue
 from tools.tools import load_setting
+from function.datebase_user import get_user_name
+from data.message.group_message_info import GroupMessageInfo
+from function.database_group import GetGroupName
 
 
 class Schedule:
@@ -32,13 +35,23 @@ class Schedule:
                 if i.judge(messageInfo):
                     if i.applicationType != ApplicationType.META:
                         if not load_setting("debug_mode", False):
-                            print(
-                                f"触发应用: {i.applicationInfo.name}: {i.applicationInfo.info}"
-                            )
+                            if isinstance(messageInfo, GroupMessageInfo):
+                                print(
+                                    f"触发应用: {get_user_name(messageInfo.senderId,messageInfo.groupId)}({messageInfo.senderId}) {GetGroupName(messageInfo.groupId)}({messageInfo.groupId}):{i.applicationInfo.name}: {i.applicationInfo.info}"
+                                )
+                            else:
+                                print(
+                                    f"触发应用: {i.applicationInfo.name}: {i.applicationInfo.info}"
+                                )
                         else:
-                            logging.info(
-                                f"触发应用: {i.applicationInfo.name}: {i.applicationInfo.info}"
-                            )
+                            if isinstance(messageInfo, GroupMessageInfo):
+                                logging.info(
+                                    f"触发应用: {get_user_name(messageInfo.senderId,messageInfo.groupId)}({messageInfo.senderId}) {GetGroupName(messageInfo.groupId)}({messageInfo.groupId}):{i.applicationInfo.name}: {i.applicationInfo.info}"
+                                )
+                            else:
+                                logging.info(
+                                    f"触发应用: {i.applicationInfo.name}: {i.applicationInfo.info}"
+                                )
                     from functools import partial
 
                     match i.applicationCostType:
